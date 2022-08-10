7b5824f44878a4da07a15597604f7bbd53440b54
Costeira <joaocurveira13@gmail.com>
2022-08-10 22:24:10 +0100

    comment functionality

--- components/Post.js
+++ components/Post.js
@@ -6,10 +6,35 @@ import {
      HeartIcon,
      PaperAirplaneIcon 
     } from "@heroicons/react/outline";
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
+import { addDoc, collection, serverTimestamp, onSnapshot, query, orderBy } from '@firebase/firestore';
+import { db } from '../firebase';
+import Moment from 'react-moment';
 
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
 
 const Post = ({ id, username, userImg, img, caption }) => {
+
+    const { data : session }= useSession();
+    const [comment,setComment] = useState("");
+    const [comments,setComments] = useState([]);
+
+    useEffect(()=> onSnapshot(query(collection(db,'posts',id,'comments'),orderBy('timestamp','desc')),snapshot => setComments(snapshot.docs)),[db])
+
+    const sendComment = async (e) => {
+        e.preventDefault();
+        const commentToSend = comment;
+        setComment('');
+
+        await addDoc(collection(db,'posts',id, 'comments'),{
+            comment : commentToSend,
+            username : session.user.username,
+            userImage : session.user.image,
+            timestamp : serverTimestamp()
+        })
+    }
+
 
     return(
         <div className="bg-white my-7 border rounded-sm">
@@ -22,29 +47,51 @@ const Post = ({ id, username, userImg, img, caption }) => {
             {/**img */}
             <img src={img} className="object-cover w-full" alt={`Post by ${username}`} />
             {/**Buttons */}
-            <div className="flex justify-between px-4 pt-4">
-                <div className="flex space-x-4">
-                    <HeartIcon className="btn"/>
-                    <ChatIcon className="btn"/>
-                    <PaperAirplaneIcon className="btn"/>
+            { session && 
+                <div className="flex justify-between px-4 pt-4">
+                    <div className="flex space-x-4">
+                        <HeartIcon className="btn"/>
+                        <ChatIcon className="btn"/>
+                        <PaperAirplaneIcon className="btn"/>
+                    </div>
+                    <BookmarkIcon className="btn"/>
                 </div>
-                <BookmarkIcon className="btn"/>
-            </div>
+            }
             {/**Captions */}
             <p className="p-5 truncate">
                 <span className="font-bold mr-1">{username} </span> {caption}
             </p>
             {/**comments */}
+            {comments.length > 0 && (
+                <div className="ml-10 h-20 overflow-y-scroll scrollbar-thumb-black scrollbar-thin">
+                    {comments.map(c => 
+                        <div key={c.id} className="flex items-center space-x-2 mb-3">
+                            <img 
+                                className="h-7 rounded-full"
+                                src={c.data().userImage} 
+                            />
+                            <p className="text-sm flex-1"><span className="font-bold">{c.data().username} </span>{c.data().comment}</p>
+                            <Moment fromNow className="pr-5 text-xs">
+                                {c.data().timestamp?.toDate()}
+                            </Moment>
+                        </div>
+                    )}
+                </div>
+            )}
             {/**input box */}
-            <form className="flex items-center p-4">
-                <EmojiHappyIcon className="h-7"/>
-                <input 
-                    type="text" 
-                    placeholder="Add a Comment..."
-                    className="border-none flex-1 focus:ring-0 outline-none"
-                />
-                <button className="font-semibold text-blue-400 hover:text-blue-600">Post</button>
-            </form>
+            {session && 
+                <form className="flex items-center p-4">
+                    <EmojiHappyIcon className="h-7"/>
+                    <input 
+                        type="text" 
+                        placeholder="Add a Comment..."
+                        className="border-none flex-1 focus:ring-0 outline-none"
+                        value={comment}
+                        onChange={({target}) => { setComment(target.value) }}
+                    />
+                    <button type="submit" disabled={!comment.trim()} onClick={sendComment} className="font-semibold text-blue-400 hover:text-blue-600">Post</button>
+                </form>
+            }
         </div>
     )
 }
--- components/Posts.js
+++ components/Posts.js
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
 import Post from "./Post"
+import { collection, onSnapshot, query, orderBy } from "@firebase/firestore";
+import { db } from '../firebase';
 
-const posts = [{
+/* const posts = [{
     id : '123',
     username : 'costeirao',
     userImg : 'https://avatars.githubusercontent.com/u/37018199',
@@ -34,13 +37,33 @@ const posts = [{
     userImg : 'https://avatars.githubusercontent.com/u/37018199',
     img : 'https://avatars.githubusercontent.com/u/37018199',
     caption : 'Costeirao wearing a bucket hat'
-}]
+}] */
+
 const Posts = () => {
 
+    const [posts,setPosts] = useState([]);
+
+    useEffect(() => 
+        onSnapshot(
+            query(collection(db, 'posts'),orderBy('timestamp','desc')),
+            snapshot =>{ setPosts(snapshot.docs) })
+    ,[db])
+
+    console.log("posts",posts)
     return(
         <div>
             {/**Iterate throu all the posts... */}
-            {posts.map( p => <Post key={p.id} {...p}/>)}
+            {posts.map( p => 
+                //<Post key={p.id} {...p}/>
+                <Post 
+                    key={p.id}
+                    id={p.id}
+                    username={p.data().username}
+                    userImg={p.data().profileImg}
+                    img={p.data().image}
+                    caption={p.data().caption}
+                />
+            )}
         </div>
     )
 }
